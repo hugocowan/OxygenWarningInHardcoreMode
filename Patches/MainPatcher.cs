47c0124479fd171d763dd5eb10e656476fe0d943
hugocowan <hugocowan@hotmail.co.uk>
2021-06-29 22:47:06 +0100

    Add project files.

---
+++ Patches/MainPatcher.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+
+namespace OxygenWarningInHardcoreMode
+{
+    [QModCore]
+    public class MainPatcher
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            Harmony harmony = new Harmony("com.boogaliwoogali.subnautica.OxygenWarningInHardcoreMode");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Patches/PlayerPatcher.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+
+namespace OxygenWarningInHardcoreMode
+{
+    [HarmonyPatch(typeof(Player))]
+    class PlayerPatcher
+    {
+
+        public static bool isPermaDeath = false;
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Player.Awake))]
+        public static void AwakePostfix()
+        {
+            if (GameModeUtils.IsOptionActive(GameModeOption.Permadeath))
+            {
+                isPermaDeath = true;
+                GameModeUtils.currentGameMode = GameModeOption.Survival;
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Player.OnKill))]
+        public static void OnKillPrefix()
+        {
+            if (isPermaDeath)
+            {
+                GameModeUtils.currentGameMode = GameModeOption.Permadeath;
+            }
+        }
+    }
+}
